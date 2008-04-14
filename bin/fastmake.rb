b5fd95c3dabd38c24498ae5225cd93707f4b9fd2
cberger <cberger>
2008-04-14 14:08:07 +0000

    make the detection of make in a global variable, this way in case of fallback, the variable countaining make is defined
    git-svn-id: svn+ssh://svn.laas.fr/svn/jafar/jafarBackbone/trunk@2890 d3dd4ecc-a6df-0310-ac42-d4bc92953605

--- bin/fastmake.rb
+++ bin/fastmake.rb
@@ -1,5 +1,26 @@
 #!/usr/bin/env ruby
 
+def initMake()
+  make_array = ["make", "gmake"]
+  
+  # check that make --version returns 0
+  # GNU Make is probably the only make which understand long options so ...
+  # If it is not the case, we will need something smarter to find which make is really GNU Make
+  
+  make_array.each do |make|
+    if (system("#{make} --version 1>/dev/null 2>/dev/null"))
+        puts "Will use #{make} as GNU Make"
+        $GNU_MAKE = make
+        return
+    end
+  end
+
+  STDERR.puts "It seems you don't have GNU Make on your system"
+  exit(-1)
+end
+
+initMake
+
 class Builder
   def initialize(option)
     f = File.new("#{ENV['JAFAR_DIR']}/Module.config")
@@ -34,12 +55,14 @@ class Builder
       optionalModules = optionalModules.split(/=/)[1]
       if(not optionalModules.nil?)
         optionalModules.split(/ /).each() { |m|
-          if(m != "" and File.exist?(moduleDir(m)))
-            if(not buildModule(m))
-              return false
+          if(m != "")
+            if(File.exist?(moduleDir(m)))
+              if(not buildModule(m))
+                return false
+              end
+            else
+              puts "Optional module #{m} not found."
             end
-          else
-            puts "Optional module #{File.exist?(moduleDir(m))} not found."
           end
         }
       end
@@ -54,30 +77,12 @@ private
     end
     if(not @buildedModule.include?(name))
       @buildedModule << name
-      return system("cd #{moduleDir(name)};#{@make} #{@option} dirs lib-install #{@scriptLanguages}")
+      return system("cd #{moduleDir(name)};#{$GNU_MAKE} #{@option} dirs lib-install #{@scriptLanguages}")
     end
     return true
   end
   def moduleDir(name)
     return "#{ENV['JAFAR_DIR']}/modules/#{name}/"
-  end
-  def getMake()
-    make_array = ["make", "gmake"]
-		
-		# check that make --version returns 0
-		# GNU Make is probably the only make which understand long options so ...
-		# If it is not the case, we will need something smarter to find which make is really GNU Make
-		
-		make_array.each do |make|
-      if (system("#{make} --version 1>/dev/null 2>/dev/null"))
-					puts "Will use #{make} as GNU Make"
-					@make = make
-					return
-			end
-		end
-
-		STDERR.puts "It seems you don't have GNU Make on your system"
-		@make = "" # Probably need to send some exception XXX
   end
 end
 
@@ -93,6 +98,6 @@ if(usefastbuild)
   b = Builder.new(ARGV.join(" "))
   exit(b.buildModule(File.basename(Dir.pwd)))
 else
-  puts "#{@make} #{ARGV.join(" ")}"
-  system("#{@make} #{ARGV.join(" ")}")
+  puts "#{$GNU_MAKE} #{ARGV.join(" ")}"
+  system("#{$GNU_MAKE} #{ARGV.join(" ")}")
 end
