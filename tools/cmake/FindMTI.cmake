bfc25f794b9b0cb08015d1b08e854b43a441c442
croussil <croussil>
2010-06-24 16:48:38 +0000

    - cleaner management of lib MTI in cmake - define macros HAVE_$LIB and HAVE_MOD_$OPTIONALMODULE instead of $LIB and $OPTIONALMODULE to avoid conflicts
    git-svn-id: svn+ssh://svn.laas.fr/svn/jafar/jafarBackbone/trunk@4627 d3dd4ecc-a6df-0310-ac42-d4bc92953605

--- tools/cmake/FindMTI.cmake
+++ tools/cmake/FindMTI.cmake
@@ -1,13 +1,4 @@
-
-macro(FIND_MTI)
-  message(STATUS "looking specifically for MTI")
-#  string(TOLOWER "${internal_library}" library_name)
-#  string(TOUPPER "${internal_library}" LIBRARY_NAME)
-#  string(TOUPPER "${internal_library}_FOUND" LIBRARY_NAME_FOUND)
- # string(TOUPPER "${internal_library}_INCLUDE_DIRS" LIBRARY_NAME_INCLUDE_DIRS)
-#  string(TOUPPER "${internal_library}_LIBRARIES" LIBRARY_NAME_LIBRARIES)
-
-  set(MTI_FOUND 0)
+# $Id: $ #
 
   find_path(MTI_INCLUDE_DIRS
     NAMES MTI.h
@@ -20,7 +11,6 @@ macro(FIND_MTI)
     /opt/local/include
     PATH_SUFFIXES MTI-clients
     DOC "The directory where MTI.h resides")
-  message(STATUS "\tMTI headers: ${MTI_INCLUDE_DIRS}")
 
   find_library(MTI_LIBRARIES
     NAMES MTI
@@ -34,11 +24,13 @@ macro(FIND_MTI)
     /sw/lib
     /opt/local/lib
     DOC "The MTI library")
-  message(STATUS "\tMTI libraries: ${MTI_LIBRARIES}")
 
-  if(EXISTS ${MTI_LIBRARIES} AND EXISTS ${MTI_INCLUDE_DIRS})
-    set(MTI_FOUND 1)
-  endif(EXISTS ${MTI_LIBRARIES} AND EXISTS ${MTI_INCLUDE_DIRS})
-  mark_as_advanced( FORCE ${MTI_FOUND} )
+  # if(EXISTS ${MTI_LIBRARIES} AND EXISTS ${MTI_INCLUDE_DIRS})
+  #   set(MTI_FOUND)
+  # endif(EXISTS ${MTI_LIBRARIES} AND EXISTS ${MTI_INCLUDE_DIRS})
+  # mark_as_advanced( FORCE MTI_FOUND )
 
-endmacro(FIND_MTI)
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(MTI  DEFAULT_MSG  MTI_LIBRARIES  MTI_INCLUDE_DIRS)
+
+MARK_AS_ADVANCED(MTI_INCLUDE_DIRS MTI_LIBRARIES)
