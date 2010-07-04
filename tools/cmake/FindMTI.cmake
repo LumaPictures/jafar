123f49bbe95160880bfe4f0f2cdd60a6a77d9b21
nksallem <nksallem>
2010-07-04 16:19:11 +0000

    Fix: Set svn properties on this file Renice: Remove commented part
    git-svn-id: svn+ssh://svn.laas.fr/svn/jafar/jafarBackbone/trunk@4661 d3dd4ecc-a6df-0310-ac42-d4bc92953605

--- tools/cmake/FindMTI.cmake
+++ tools/cmake/FindMTI.cmake
@@ -1,36 +1,31 @@
-# $Id: $ #
+# $Id$ #
 
-  find_path(MTI_INCLUDE_DIRS
-    NAMES MTI.h
-    PATHS
-    ${ROBOTPKG_BASE}/include
-    $ENV{ROBOTPKG_BASE}/include
-    /usr/include
-    /usr/local/include
-    /sw/include
-    /opt/local/include
-    PATH_SUFFIXES MTI-clients
-    DOC "The directory where MTI.h resides")
+find_path(MTI_INCLUDE_DIRS
+  NAMES MTI.h
+  PATHS
+  ${ROBOTPKG_BASE}/include
+  $ENV{ROBOTPKG_BASE}/include
+  /usr/include
+  /usr/local/include
+  /sw/include
+  /opt/local/include
+  PATH_SUFFIXES MTI-clients
+  DOC "The directory where MTI.h resides")
 
-  find_library(MTI_LIBRARIES
-    NAMES MTI
-    PATHS
-    ${ROBOTPKG_BASE}/lib
-    $ENV{ROBOTPKG_BASE}/lib
-    /usr/lib
-    /usr/lib64
-    /usr/local/lib
-    /usr/local/lib64
-    /sw/lib
-    /opt/local/lib
-    DOC "The MTI library")
+find_library(MTI_LIBRARIES
+  NAMES MTI
+  PATHS
+  ${ROBOTPKG_BASE}/lib
+  $ENV{ROBOTPKG_BASE}/lib
+  /usr/lib
+  /usr/lib64
+  /usr/local/lib
+  /usr/local/lib64
+  /sw/lib
+  /opt/local/lib
+  DOC "The MTI library")
 
-  # if(EXISTS ${MTI_LIBRARIES} AND EXISTS ${MTI_INCLUDE_DIRS})
-  #   set(MTI_FOUND)
-  # endif(EXISTS ${MTI_LIBRARIES} AND EXISTS ${MTI_INCLUDE_DIRS})
-  # mark_as_advanced( FORCE MTI_FOUND )
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(MTI  DEFAULT_MSG  MTI_LIBRARIES  MTI_INCLUDE_DIRS)
 
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(MTI  DEFAULT_MSG  MTI_LIBRARIES  MTI_INCLUDE_DIRS)
-
-MARK_AS_ADVANCED(MTI_INCLUDE_DIRS MTI_LIBRARIES)
+mark_as_advanced(MTI_INCLUDE_DIRS MTI_LIBRARIES)
