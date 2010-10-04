d94c9f918c1e60bbc606da11e565b7ee0fd1ebce
Arnaud Degroote <arnaud.degroote@laas.fr>
2010-10-04 10:10:57 +0200

    Add script to add module in the git hierarchy

---
+++ bin/add_module.sh
@@ -0,0 +1,76 @@
+#! /bin/sh
+#set -e
+
+. ${JAFAR_DIR}/bin/git_config.sh
+
+usage()
+{
+    cat <<EOF
+	add_module [name]
+
+    imports the jafar module 'name' 
+
+The script adds C, C++, documentation, Swig and scripts and sets the needed svn properties. 
+You have to add any other files (if any) manually
+EOF
+    exit 1
+}
+
+MODULE=`echo $1 | sed 's,/$,,'`
+test -z "$MODULE" && usage;
+
+if ! test -d "$MODULE"; then
+    echo "ERROR: $MODULE is not a directory"
+    exit 1
+fi
+
+# Check if a module of same kind exists
+# Otherwise, create the repository
+# For this, call create_jafar_git.sh, with $1 = $MODULE
+sed "s,\${1},$MODULE,"  ${JAFAR_DIR}/bin/create_jafar_git.sh | ssh ${VCS_USER}@${HOST} "sh -s"
+
+if [ $? -ne 0 ]
+then
+	exit 1
+fi
+
+TOPDIR=$PWD/$MODULE
+
+cd $TOPDIR
+git init 
+
+git add COPYRIGHT Makefile README User.make doc include macro src test_suite
+
+cd $TOPDIR/doc
+git add *.doxy *.tcl *.hpp *.cpp
+
+cd $TOPDIR/include
+git add *.i
+
+cd $TOPDIR/include/$MODULE
+git add *.hpp *.h *.i
+
+cd $TOPDIR/macro
+git add *.tcl *.rb
+
+cd $TOPDIR/src
+git add *.cpp *.c ruby
+
+cd $TOPDIR/src/ruby
+git add extconf.rb
+
+cd $TOPDIR/test_suite
+git add *.cpp *.hpp
+
+cd $TOPDIR
+
+git commit -m "Initial commit of ${MODULE}"
+
+git push ${BASE_URL}/${MODULE} master
+
+echo "[remote \"origin\"]
+	url = ${BASE_URL}/${MODULE}
+	fetch = +refs/heads/*:refs/remotes/origin/*
+[branch \"master\"]
+	remote = origin
+	merge = refs/heads/master" >> ${TOPDIR}/.git/config
---
+++ bin/create_jafar_git.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+
+BASE_GIT=/git/robots
+BASE_DIRECTORY=${BASE_GIT}/jafar/modules/test/
+
+MODULE_DIRECTORY="${BASE_DIRECTORY}${1}.git"
+if [ -d ${MODULE_DIRECTORY} ]
+then
+	echo "The module ${1} seems to already exists !!"
+	exit 1
+fi
+
+
+mkdir ${MODULE_DIRECTORY}
+cd ${MODULE_DIRECTORY}
+git --bare init --shared
+
+# description
+echo "jafar/module/${1}" > ${MODULE_DIRECTORY}/description
+
+# Add hooks
+rm ${MODULE_DIRECTORY}/hooks/post-receive
+ln -s ${BASE_GIT}/laas-git-hooks.git/post-receive ${MODULE_DIRECTORY}/hooks/
+
+# Add config hooks
+echo "[hooks]" >> ${MODULE_DIRECTORY}/config
+echo "        emailprefix = " >> ${MODULE_DIRECTORY}/config
+echo "        mailinglist = adegroot@laas.fr" >> ${MODULE_DIRECTORY}/config
