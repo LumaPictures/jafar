efc2cf4c54bebaf3ca37ef955def136d5a0e0f36
Cyril Roussillon <cyril.roussillon@laas.fr>
2010-10-19 11:28:11 +0200

    Fix in get_or_update_all_modules.sh: also pull jafar's backbone

--- bin/get_or_update_all_modules.sh
+++ bin/get_or_update_all_modules.sh
@@ -4,18 +4,21 @@ SCRIPT_PATH=$(cd ${0%/*} && echo $PWD/${0##*/})
 MY_JAFAR_DIR=`dirname "$SCRIPT_PATH"`/..
 
 . ${MY_JAFAR_DIR}/bin/git_config.sh
-cd ${MY_JAFAR_DIR}/modules
+cd ${MY_JAFAR_DIR}
+git pull --rebase
+
+cd modules
 
 REMOTE_MODULES=`ssh ${VCS_USER}@${HOST} "sh -c 'cd ${BASE_DIRECTORY} ; ls -d */ | sed s,.git/,,'"`
 
 for m in $REMOTE_MODULES; do
 	if [ -d $m ]; then
-		echo "Pulling $m"
+		echo "### Pulling $m"
 		cd $m
 		git pull --rebase
 		cd -
 	else
-		echo "Cloning $m"
+		echo "### Cloning $m"
 		git clone $BASE_URL$m
 	fi;
 
