26c159cf3c8132ccaefaf155828d83120cd6c3b6
Nizar Sallem <nksallem@laas.fr>
2011-01-13 09:14:51 +0100

    Update: Be more restrictive and impose headers as a condition two

--- tools/cmake/FindOpenNIX.cmake
+++ tools/cmake/FindOpenNIX.cmake
@@ -1,19 +1,4 @@
 message(STATUS "looking for opennix")
-# set(OPENNI_NITE_INCLUDE_DIR)
-# set(OPENNI_INCLUDE_DIR)
-# set(OPENNI_FOUND 0)
-
-find_path(OPENNI_NITE_INCLUDE_DIR
-  NAMES XnVNite.h
-  PATHS
-  ${ROBOTPKG_BASE}/include
-  $ENV{ROBOTPKG_BASE}/include
-  /usr/include
-  /usr/local/include
-  /sw/include
-  /opt/local/include
-  PATH_SUFFIXES nite 
-  DOC "The directory where XnVNite.h resides")
 
 find_path(OPENNI_INCLUDE_DIR
   NAMES XnOpenNI.h
@@ -26,8 +11,31 @@ find_path(OPENNI_INCLUDE_DIR
   /opt/local/include
   PATH_SUFFIXES ni
   DOC "The directory where XnOpenNI.h resides")
+if(EXISTS ${OPENNI_INCLUDE_DIR})
+	set(OPENNI_INCLUDE_DIR_FOUND 1)
+	mark_as_advanced(FORCE OPENNI_INCLUDE_DIR)
+endif()
 
-SET(OPENNIX_INCLUDE_DIRS ${OPENNI_NITE_INCLUDE_DIR};${OPENNI_INCLUDE_DIR})
+find_path(OPENNI_NITE_INCLUDE_DIR
+  NAMES XnVNite.h
+  PATHS
+  ${ROBOTPKG_BASE}/include
+  $ENV{ROBOTPKG_BASE}/include
+  /usr/include
+  /usr/local/include
+  /sw/include
+  /opt/local/include
+  PATH_SUFFIXES nite 
+  DOC "The directory where XnVNite.h resides")
+if(EXISTS ${OPENNI_NITE_INCLUDE_DIR})
+	set(OPENNI_NITE_INCLUDE_DIR_FOUND 1)
+	mark_as_advanced(FORCE OPENNI_NITE_INCLUDE_DIR)
+endif()
+
+if(OPENNI_INCLUDE_DIR_FOUND AND OPENNI_NITE_INCLUDE_DIR_FOUND)
+	SET(OPENNIX_INCLUDE_DIRS ${OPENNI_NITE_INCLUDE_DIR};${OPENNI_INCLUDE_DIR})
+	SET(OPENNIX_INCLUDE_DIRS_FOUND 1)
+endif()
 
 message(STATUS "\topennix headers: ${OPENNIX_INCLUDE_DIRS}")
 
@@ -43,6 +51,10 @@ find_library(OPENNI_LIBRARIES
   /sw/lib
   /opt/local/lib
   DOC "The OpenNI library")
+if(EXISTS ${OPENNI_LIBRARIES})
+	set(OPENNI_LIBRARIES_FOUND 1)
+	mark_as_advanced(FORCE OPENNI_LIBRARIES)
+endif()
 
 find_library(OPENNI_NITE_LIBRARIES
   NAMES XnVNite XnVFeatures XnVHandGenerator
@@ -56,6 +68,10 @@ find_library(OPENNI_NITE_LIBRARIES
   /sw/lib
   /opt/local/lib
   DOC "The OpenNI Nite library")
+if(EXISTS ${OPENNI_NITE_LIBRARIES})
+	set(OPENNI_NITE_LIBRARIES_FOUND 1)
+	mark_as_advanced(FORCE OPENNI_NITE_LIBRARIES)
+endif()
 
 find_library(OPENNI_SENSOR_LIBRARIES
   NAMES XnCore XnDDK XnDeviceFile XnDeviceSensorV2 XnFormats
@@ -69,10 +85,20 @@ find_library(OPENNI_SENSOR_LIBRARIES
   /sw/lib
   /opt/local/lib
   DOC "The OpenNI Sensor library")
+if(EXISTS ${OPENNI_SENSOR_LIBRARIES})
+	set(OPENNI_SENSOR_LIBRARIES_FOUND 1)
+	mark_as_advanced(FORCE OPENNI_SENSOR_LIBRARIES)
+endif()
 
-SET(OPENNIX_LIBRARIES ${OPENNI_NITE_LIBRARIES};${OPENNI_LIBRARIES};${OPENNI_SENSOR_LIBRARIES})
+if(OPENNI_LIBRARIES_FOUND AND 
+	 OPENNI_SENSOR_LIBRARIES_FOUND AND 
+	 OPENNI_NITE_LIBRARIES_FOUND)
+ SET(OPENNIX_LIBRARIES ${OPENNI_NITE_LIBRARIES};${OPENNI_LIBRARIES};${OPENNI_SENSOR_LIBRARIES})
+ SET(OPENNIX_LIBRARIES_FOUND 1)
+endif()
 message(STATUS "\topennix libraries: ${OPENNIX_LIBRARIES}")
-if(OPENNIX_FOUND)
+
+if(OPENNIX_LIBRARIES_FOUND AND OPENNIX_INCLUDE_DIRS_FOUND)
   set(OPENNIX_FOUND 1)
 else()
   set(OPENNIX_FOUND 0)
