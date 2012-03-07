01b8b71e46f745cc97e4c9f64113964ce69f0a91
Cyril Robin <crobin@laas.fr>
2012-03-07 11:44:10 +0100

    Add demo_suite in template_module

---
+++ share/template_module/demo_suite/demo__jfr_module_.cpp
@@ -0,0 +1,17 @@
+/** 
+ * This is a demo file
+ * Fill it as you like for other users!
+ *
+ * Feel free to add some other specific demo files too.
+ *
+ */
+
+#include <iostream>
+
+int main(int argc, char ** argv) 
+{
+	std::cout << "Welcome to the demo showroom !" << std::endl ;
+
+	return 0;
+}
+
